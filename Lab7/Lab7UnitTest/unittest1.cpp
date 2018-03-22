986b88de64ea95122d675e6bfac8d4dfde8329b4
Jae Jang <jaejang5566@gmail.com>
2018-03-22 15:14:35 -0700

    more tests

--- Lab7/Lab7UnitTest/unittest1.cpp
+++ Lab7/Lab7UnitTest/unittest1.cpp
@@ -9,7 +9,7 @@ namespace Lab7UnitTest
 	{
 	public:
 		
-		TEST_METHOD(SumTest)
+		TEST_METHOD(SumTest1)
 		{
 			std::vector<int> a;
 			a.push_back(1);
@@ -22,6 +22,22 @@ namespace Lab7UnitTest
 			a.push_back(10);
 
 			Assert::AreEqual(44, sum(a));
+
+		}
+
+		TEST_METHOD(SumTest2)
+		{
+			std::vector<int> a;
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(10);
+
+			Assert::AreEqual(45, sum(a));
 
 		}
 
@@ -40,6 +56,22 @@ namespace Lab7UnitTest
 			Assert::AreEqual(11, max(a));
 
 		}
+
+		TEST_METHOD(MaxTest2)
+		{
+			std::vector<int> a;
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+
+			Assert::AreEqual(5, max(a));
+
+		}
 		TEST_METHOD(MinTest)
 		{
 			std::vector<int> a;
@@ -56,6 +88,22 @@ namespace Lab7UnitTest
 
 		}
 
+		TEST_METHOD(MinTest2)
+		{
+			std::vector<int> a;
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+
+			Assert::AreEqual(5, min(a));
+
+		}
+
 		TEST_METHOD(AverageTest)
 		{
 			std::vector<int> a;
@@ -69,6 +117,22 @@ namespace Lab7UnitTest
 			a.push_back(10);
 
 			Assert::AreEqual(5.5, average(a));
+
+		}
+
+		TEST_METHOD(AverageTest2)
+		{
+			std::vector<int> a;
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+			a.push_back(5);
+
+			Assert::AreEqual(5.0, average(a));
 
 		}
 	};
